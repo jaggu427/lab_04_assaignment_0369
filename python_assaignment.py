80ace7c403afac022f811fca4a284f668aa5b2e9
jaggu427 <120269332+jaggu427@users.noreply.github.com>
2023-08-24 06:13:56 +0000

    msg_3

--- python_assaignment.py
+++ python_assaignment.py
@@ -1,3 +1,71 @@
 # i have created a python file
 
 #my roll no is e22cseu0369
+
+class Match:
+    def __init__(self, location, team1, team2, timing):
+        self.location = location
+        self.team1 = team1
+        self.team2 = team2
+        self.timing = timing
+
+class FlightTable:
+    def __init__(self):
+        self.matches = []
+
+    def add_match(self, match):
+        self.matches.append(match)
+
+    def search_by_team(self, team_name):
+        matching_matches = [match for match in self.matches if team_name in [match.team1, match.team2]]
+        return matching_matches
+
+    def search_by_location(self, location):
+        matching_matches = [match for match in self.matches if match.location == location]
+        return matching_matches
+
+    def search_by_timing(self, timing):
+        matching_matches = [match for match in self.matches if match.timing == timing]
+        return matching_matches
+
+def main():
+    flight_table = FlightTable()
+
+    flight_table.add_match(Match("Mumbai", "India", "Sri Lanka", "DAY"))
+    flight_table.add_match(Match("Delhi", "England", "Australia", "DAY-NIGHT"))
+    flight_table.add_match(Match("Chennai", "India", "South Africa", "DAY"))
+    flight_table.add_match(Match("Indore", "England", "Sri Lanka", "DAY-NIGHT"))
+    flight_table.add_match(Match("Mohali", "Australia", "South Africa", "DAY-NIGHT"))
+    flight_table.add_match(Match("Delhi", "India", "Australia", "DAY"))
+
+    while True:
+        print("Choose a search parameter:")
+        print("1. List of all the matches of a Team")
+        print("2. List of Matches on a Location")
+        print("3. List of Matches based on timing")
+        print("4. Exit")
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            team_name = input("Enter the team name: ")
+            matches = flight_table.search_by_team(team_name)
+            for match in matches:
+                print(f"Location: {match.location}, Teams: {match.team1} vs {match.team2}, Timing: {match.timing}")
+
+        elif choice == 2:
+            location = input("Enter the location: ")
+            matches = flight_table.search_by_location(location)
+            for match in matches:
+                print(f"Location: {match.location}, Teams: {match.team1} vs {match.team2}, Timing: {match.timing}")
+
+        elif choice == 3:
+            timing = input("Enter the timing: ")
+            matches = flight_table.search_by_timing(timing)
+            for match in matches:
+                print(f"Location: {match.location}, Teams: {match.team1} vs {match.team2}, Timing: {match.timing}")
+
+        elif choice == 4:
+            break
+
+        else:
+            print("Invalid choice. Please choose another option")
